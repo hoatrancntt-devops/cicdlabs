6b6fa44c552c3b7a9afbeffc2440758b1d63c7f2
nhanjs <nhan.htmljs@gmail.com>
2025-09-20 09:04:01 +0000

    Add health api

--- nodejs-app/app.js
+++ nodejs-app/app.js
@@ -1,18 +1,21 @@
-const http = require('http');
+import express from 'express';
+const app = express();
 
 const hostname = '0.0.0.0';
 const port = 3000;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/html');
-  res.end(`<!DOCTYPE html>
+app.get('/', (req, res) => {
+  res.status(200).send(`<!DOCTYPE html>
 <html>
   <head><title>Node.js App</title></head>
   <body><h1>Hello from Node.js on Kubernetes!</h1></body>
 </html>`);
 });
 
-server.listen(port, hostname, () => {
+app.get('/health', (req, res) => {
+  res.status(200).json({ status: 'ok' });
+});
+
+app.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
